2d825a6fa599ae18a0b6b131633a6cca9fa816cc
boltang2 <boltang22@gmail.com>
2023-03-14 15:54:14 +0900

    0314

--- Programmers/src/experiment/Experiment.java
+++ Programmers/src/experiment/Experiment.java
@@ -3,33 +3,17 @@ package experiment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Experiment {
 	public static void main(String[] args) {
-		int[] data = {4,3,2,1,5,2,3,2,5,1,5};
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		HashMap<Integer, Integer> tmp = new HashMap<Integer, Integer>();
-		map.put(2,2);
-		map.put(3,3);
-		map.put(4,4);
+		HashSet<int[]> a = new HashSet<int[]>();
+
+		a.add(new int[] { 1, 0 });
+		a.add(new int[] { 1, 0 });
 		
-		ArrayList<Integer> keys = new ArrayList<Integer>();
-		for(int key : map.keySet()) {
-			keys.add(key);
-		}
-		System.out.println(keys);
-		for (int keyA : keys) {
-			map.remove(keyA);
-			for(int keyB : map.keySet()) {
-				System.out.print(keyA + " ");
-				System.out.println(keyB);
-			}
-		}
-		
-		String a = "aavvcc";
-		System.out.println(a);
-		 = "d";
-		System.out.println(a);
+		System.out.println(a.size());
+		String b = 1 + "234";
+		System.out.println(b);
 	}
 }
-
